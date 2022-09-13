63cb69e25e574336148f9826cd57b95a449eca9a
Naresh015 <102531959+Naresh015@users.noreply.github.com>
2022-09-13 19:33:08 +0530

    import java.util.Scanner; class primeinrange {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=(a*(a+1)/2);
            System.out.println(b);
        }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class primeinrange
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=(a*(a+1)/2);
+       
+        System.out.println(b);
+    }
+}
